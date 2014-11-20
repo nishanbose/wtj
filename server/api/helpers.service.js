7e38560ad2f6d47d53677c97613816909479b5fe
Larry Siden <lsiden@gmail.com>
2014-11-20 12:58:42 -0500

    /api - helpers.service

---
+++ server/api/helpers.service.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// var mongoose = require('mongoose');
+// var passport = require('passport');
+// var config = require('../config/environment');
+// var jwt = require('jsonwebtoken');
+// var expressJwt = require('express-jwt');
+// var compose = require('composable-middleware');
+// var User = require('../api/user/user.model');
+// var validateJwt = expressJwt({ secret: config.secrets.session });
+var _ = require('lodash')
+var tracer = require('tracer').console({ level: 'info' });
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+function processQuery(_query) {
+  var query = _.cloneDeep(_query);
+  tracer.debug(query);
+  for (var key in query) {
+    if (_.isArray(query[key])) {
+      query[key] = (key === '_id[]') ? { $in: query[key].map(toObjectId) } : { $in: query[key] };
+    }
+  }
+  tracer.debug(query);
+  return query;
+}
+
+function handleError(res, err) {
+  // tracer.error(err);
+  console.log(err);
+  return res.send(500, err);
+}
+
+function toObjectId(id) { 
+  return Schema.Types.ObjectId(id)
+}
+
+exports.processQuery = processQuery;
+exports.handleError = handleError;
+exports.toObjectId = toObjectId;
--- server/api/list/list.controller.js
+++ server/api/list/list.controller.js
@@ -6,6 +6,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = require('mongoose').Types.ObjectId;
 var auth = require('../../auth/auth.service');
+var helpers = require('../helpers.service');
 
 // Get list of lists
 exports.index = function(req, res) {
@@ -20,9 +21,9 @@ exports.index = function(req, res) {
   if (top) { q.limit(top); }
   // mongoose.set('debug', true);
   q.exec(function (err, lists) {
-    if(err) { return handleError(res, err); }
+    if(err) { return helpers.handleError(res, err); }
     List.populate(lists, { path: 'categories author', select: '_id name email' }, function(err, popList) {
-      if(err) { return handleError(res, err); }
+      if(err) { return helpers.handleError(res, err); }
       return res.json(200, lists);
     });
   });
@@ -31,10 +32,10 @@ exports.index = function(req, res) {
 // Get a single list
 exports.show = function(req, res) {
   List.findById(req.params.id, function (err, list) {
-    if(err) { return handleError(res, err); }
+    if(err) { return helpers.handleError(res, err); }
     if(!list) { return res.send(404); }
     List.populate(list, { path: 'categories author', select: '_id name email' }, function(err, popList) {
-      if(err) { return handleError(res, err); }
+      if(err) { return helpers.handleError(res, err); }
       return res.json(200, list);
     });
   });
@@ -45,7 +46,7 @@ exports.create = function(req, res) {
   req.body.author = _.clone(req.user._doc);
   console.log(req.body);
   List.create(req.body, function(err, list) {
-    if(err) { return handleError(res, err); }
+    if(err) { return helpers.handleError(res, err); }
     return res.json(201, list);
   });
 };
@@ -56,11 +57,11 @@ exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
   if(req.body.author) { delete req.body.author; } // not allowed
   List.findById(req.params.id, function (err, list) {
-    if (err) { return handleError(res, err); }
+    if (err) { return helpers.handleError(res, err); }
     if(!list) { return res.send(404); }
     var updated = _.merge(list, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) { return helpers.handleError(res, err); }
       return res.json(200, list);
     });
   });
@@ -69,15 +70,11 @@ exports.update = function(req, res) {
 // Deletes a list from the DB.
 exports.destroy = function(req, res) {
   List.findById(req.params.id, function (err, list) {
-    if(err) { return handleError(res, err); }
+    if(err) { return helpers.handleError(res, err); }
     if(!list) { return res.send(404); }
     list.remove(function(err) {
-      if(err) { return handleError(res, err); }
+      if(err) { return helpers.handleError(res, err); }
       return res.send(204);
     });
   });
 };
-
-function handleError(res, err) {
-  return res.send(500, err);
-}
