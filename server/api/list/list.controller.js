99891f91d86ac9f2a71de30030fcfc233d654d89
Larry Siden <lsiden@gmail.com>
2014-11-26 01:19:38 -0500

    /list/:id - Attempt to send mail notification when Admin blocks or unblocks
    not yet working

--- server/api/list/list.controller.js
+++ server/api/list/list.controller.js
@@ -69,7 +69,7 @@ exports.create = function(req, res) {
 
 // Updates an existing list in the DB.
 exports.update = function(req, res) {
-  var tracer = require('tracer').console({ level: 'warn' });
+  var tracer = require('tracer').console({ level: 'debug' });
   
   if (!auth.hasRole('admin') && req.user._id !== req.body.author._id) {return res.send(401); }
   if(req.body._id) { delete req.body._id; }
@@ -79,15 +79,33 @@ exports.update = function(req, res) {
     return _.has(cat, '_id') ? cat._id : cat;
   });
 
-  List.findById(req.params.id, function (err, list) {
+  List.findById(req.params.id)
+  .populate({ path: 'categories author', select: '_id name email' })
+  .exec(function(err, list) {
     if (err) { return helpers.handleError(res, err); }
-    if(!list) { return res.send(404); }
-  
-    // var updated = _.merge(list, req.body);
-    var updated = _.assign(list, req.body);
-    tracer.trace('updating ', updated);
-    // updated.save(function (err, saved) {
-    updated.save(function (err) {
+    if (!list) { return res.send(404); }
+
+    var wasActive = _.clone(list.active);
+    _.assign(list, req.body);
+
+    if (typeof list.active !== 'undefined' && list.active != wasActive) {
+      var mandrill = require('../../components/mail/mandrill.service');
+      var message;
+      if (list.active) {
+        message = 'The list :title has been un-blocked.';
+      } else {
+        message = 'The list :title has been blocked because it does not comply with community standards.  If you have questions, you may reply to the site administrator.'.replace(/:title/, list.title);
+      }
+      tracer.debug(list.author);
+      var to = [{
+        email: list.author.email,
+        name: list.author.name
+      }];
+      mandrill.send(to, list.title, message);
+    }
+    tracer.log('updating');
+    tracer.log(list);
+    list.save(function (err) {
       if (err) { return helpers.handleError(res, err); }
       return res.json(200, list);
     });
