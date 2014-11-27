3d930a3fe80467fff1164acc1fa0bc972a1b3eb6
Larry Siden <lsiden@gmail.com>
2014-11-27 08:29:32 -0500

    /api/complaints

---
+++ server/api/complaint/complaint.model.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var ComplaintSchema = new Schema({
+  list: { type: ObjectId, ref: 'List', index: true, required: true },
+  source: { type: String, index: true }, // IP of source
+  reason: String
+});
+
+var timestamps = require('mongoose-timestamp');
+ComplaintSchema.plugin(timestamps);
+
+module.exports = mongoose.model('Complaint', ComplaintSchema);
